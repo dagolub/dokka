426f231ad8e8b2494a58f24dc0d5ea342c20d635
Dmitriy Golub <=>
2021-01-02 16:35:20 +0200

    fix

--- services/web/manage.py
+++ services/web/manage.py
@@ -1,9 +1,10 @@
-from flask import Flask, jsonify
+from flask.cli import FlaskGroup
+
+from project import app
 
 
-app = Flask(__name__)
+cli = FlaskGroup(app)
 
 
-@app.route("/")
-def hello_world():
-    return jsonify(hello="world")
+if __name__ == "__main__":
+    cli()
